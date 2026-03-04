2407d8ab422e9ebd72d19c8ba981784fdbfc8d18
Zafer Külte <zaferkulte@gmail.com>
2026-03-04 13:56:14 +0300

    Fix lint warnings in outputs and absent manager

--- src/components/OutputsSection.jsx
+++ src/components/OutputsSection.jsx
@@ -11,7 +11,6 @@ export default function OutputsSection({
   teachersForCurrentDay,
   classes,
   classAbsence,
-  filteredClassAbsence,
   absentPeopleForCurrentDay,
   commonLessons,
   onExportJPG,
